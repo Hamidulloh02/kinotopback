f16da5766d125cb085f30d11369cec98ab210822
Hamidjonn <hamidullonematullayev22@gmail.com>
2022-07-04 12:57:26 +0500

    first api

---
+++ posts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+from .views import PostViewSet, UserViewSet
+
+router = SimpleRouter()
+router.register('user', UserViewSet, basename='user')
+router.register('', PostViewSet, basename='post')
+
+urlpatterns = router.urls
